88af44a0748e366d8512655c5ab2666433ff27c2
HunterCartier1337 <huntercartier2@hotmail.com>
2024-10-12 10:33:47 -0700

    Create bhound.sh

---
+++ bhound.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Quick script so you don't have to remember blood-hound syntax
+# Need dock-compose installed for last line which will start docker instance: 
+# sudo apt update && sudo apt install docker-compose -y
+
+read -p "Domain (ex. Company.local): " domain
+
+read -p "Username: " username
+
+read -p "Password: " password
+
+read -p "Target IP: " ip 
+
+echo "[+] bloodhound-python -d $domain -u $username -p $password -ns $ip -c all
+bloodhound-python -d $domain -u $username -p $password -ns $ip -c all
+
+sleep 2
+
+curl -L https://ghst.ly/getbhce | sudo docker compose -f - up
+
