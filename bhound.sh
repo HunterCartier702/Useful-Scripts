d61791f5b3450d864669c77377e1816e812263cc
HunterCartier1337 <huntercartier2@hotmail.com>
2024-10-12 11:05:20 -0700

    Update bhound.sh

--- bhound.sh
+++ bhound.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Quick script so you don't have to remember blood-hound syntax
-# Need dock-compose installed for last line which will start docker instance: 
+# Need docker-compose installed for last line which will start docker instance: 
 # sudo apt update && sudo apt install docker-compose -y
+# Verbose output hence the echoed commands
 
 read -p "Domain (ex. Company.local): " domain
 
@@ -17,6 +18,7 @@ bloodhound-python -d $domain -u $username -p $password -ns $ip -c all
 
 sleep 2
 
-# Starts BloodHound Community Edition with Docker Compose 
+# Starts BloodHound Community Edition with Docker Compose   
+echo "[+] curl -L https://ghst.ly/getbhce | sudo docker compose -f - up"
 curl -L https://ghst.ly/getbhce | sudo docker compose -f - up
 
