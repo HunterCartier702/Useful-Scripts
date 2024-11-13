2a6610d286c45c6ac81e0d656592f51e689f29f9
Hunter Cartier <huntercartier2@hotmail.com>
2024-11-13 10:50:59 -0800

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+cat <<'EOF'
+       __              __________________ 
+      / _|             | ___ \  _  \ ___ \
+__  _| |_ _ __ ___  ___| |_/ / | | | |_/ /
+\ \/ /  _| '__/ _ \/ _ \    /| | | |  __/ 
+ >  <| | | | |  __/  __/ |\ \| |/ /| |    
+/_/\_\_| |_|  \___|\___\_| \_|___/ \_|    
+                                                                                    	
+EOF
+
 # Quick script to use xfreerdp to remote into windows machines with or without a drive attached
 
 read -p "IP: " IP
