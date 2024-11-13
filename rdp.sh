9ecab591aecccbceea1a5df1e20362268a32f606
Hunter Cartier <huntercartier2@hotmail.com>
2024-11-13 09:21:50 -0800

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -3,15 +3,15 @@
 # Quick script to use xfreerdp to remote into windows machines with or without a drive attached
 
 read -p "IP: " IP
-read -p "Username: " USER
-read -p "Password: " PASS
+read -p "Username: " user
+read -p "Password: " pass
 #Example /home/$USER/skripts
-read -p "Path on local box files to upload to target host(Optional Press Enter to Continue): " SHAREPATH
+read -p "/drive: (Optional) Press Enter to Continue: " SHAREPATH
 
 if [ -z "$SHAREPATH" ]; then
-	echo "[+] xfreerdp /v:$IP /u:$USER /p:$PASS /dynamic-resolution"
-	xfreerdp /v:$IP /u:$USER /p:$PASS /dynamic-resolution
+	echo "[+] xfreerdp /v:$IP /u:$user /p:$pass /dynamic-resolution"
+	xfreerdp /v:$IP /u:$user /p:$pass /dynamic-resolution
 else
-	echo "[+] xfreerdp /v:$IP /u:$USER /p:$PASS /dynamic-resolution /drive:New_Share,$SHAREPATH"
-	xfreerdp /v:$IP /u:$USER /p:$PASS /dynamic-resolution /drive:New_Share,$SHAREPATH
+	echo "[+] xfreerdp /v:$IP /u:$user /p:$pass /dynamic-resolution /drive:New_Share,$SHAREPATH"
+	xfreerdp /v:$IP /u:$user /p:$pass /dynamic-resolution /drive:New_Share,$SHAREPATH
 fi
