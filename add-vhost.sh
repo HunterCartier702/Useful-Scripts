755dc838fe04f1ddb881958e3438efb35dbaba2f
Hunter Cartier <huntercartier2@hotmail.com>
2025-01-21 11:02:39 -0800

    Update add-vhost.sh

--- add-vhost.sh
+++ add-vhost.sh
@@ -2,5 +2,8 @@
 
 read -p "Enter IP: " IP
 read -p "Enter or copy/paste vhosts list: " vhosts
-
+cat /etc/hosts
+echo "------"
 printf "%s\t%s\n\n" "$IP" "$vhosts" | sudo tee -a /etc/hosts
+echo "------"
+cat /etc/hosts
